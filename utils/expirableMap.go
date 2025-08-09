7ecc06e2a75d3989757d5a3600ad222113180c03
Kyle Measner <kyle.measner@gmail.com>
2025-08-09 14:29:01 -0700

    Fixed bug where concurrent reads and writes to ExpirableMaps would crash the app.

--- utils/expirableMap.go
+++ utils/expirableMap.go
@@ -1,6 +1,9 @@
 package utils
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type Expirable[V any] struct {
 	Value  V
@@ -8,8 +11,9 @@ type Expirable[V any] struct {
 }
 
 type ExpirableMap[K comparable, T any] struct {
-	Map map[K]Expirable[T]
-	TTL time.Duration
+	Map  map[K]Expirable[T]
+	TTL  time.Duration
+	Lock sync.RWMutex
 }
 
 func NewExpirableMap[K comparable, V any](ttl time.Duration) *ExpirableMap[K, V] {
@@ -20,24 +24,33 @@ func NewExpirableMap[K comparable, V any](ttl time.Duration) *ExpirableMap[K, V]
 }
 
 func (e *ExpirableMap[K, V]) Set(key K, value V) {
+	e.Lock.Lock()
 	e.Map[key] = Expirable[V]{
 		Value:  value,
 		Expiry: time.Now().Add(e.TTL),
 	}
+	e.Lock.Unlock()
 }
 
 func (e *ExpirableMap[K, V]) Get(key K) *V {
+	e.Lock.RLock()
 	value, ok := e.Map[key]
+	e.Lock.RUnlock()
 	if ok {
 		if time.Now().Before(value.Expiry) {
 			return &value.Value
 		} else {
+			e.Lock.Lock()
 			delete(e.Map, key) // Clean up expired entries
+			e.Lock.Unlock()
 		}
 	}
 	return nil
 }
 
 func (e *ExpirableMap[K, V]) Len() int {
-	return len(e.Map)
+	e.Lock.RLock()
+	length := len(e.Map)
+	e.Lock.RUnlock()
+	return length
 }
