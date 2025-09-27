b89b6694be2e966f2cf04a6645a1b6fb5f28aa12
Kyle Measner <kyle@Kyles-MacBook-Pro.local>
2025-09-27 11:32:39 -0700

    Updating startup script to build the app without root permissions, then run the built app with sudo.

--- startup.sh
+++ startup.sh
@@ -17,6 +17,13 @@ fi
 
 echo "Git pull completed successfully."
 
+echo "Building the app..."
+go build -o rpi-rgb-screen main.go
+if [ $? -ne 0 ]; then
+    echo "Build failed."
+    exit 1
+fi
+echo "Build completed successfully."
 
 echo "Starting up the app..."
-sudo go run main.go --led-slowdown-gpio=4
+sudo rpi-rgb-screen --led-slowdown-gpio=4
