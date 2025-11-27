1acc2af14902f942991392a4c0b0ebac3b0d0656
Kyle Measner <kyle@Kyles-MacBook-Pro.local>
2025-11-27 14:52:12 -0800

    Delete old log files on startup.

--- main.go
+++ main.go
@@ -20,8 +20,9 @@ func main() {
 	// Setup logging
 	err := os.MkdirAll("./logs", os.ModePerm)
 	if err != nil {
-		log.Fatalf("error creating logs director: %v", err)
+		log.Fatalf("error creating logs directory: %v", err)
 	}
+	deleteOldLogs()
 
 	logFileName := fmt.Sprintf("./logs/%s.txt", time.Now().Format(time.RFC3339))
 	logFile, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
@@ -73,4 +74,31 @@ func clearScreenOnExit(matrix rgbmatrix.Matrix) {
 	matrix.Close()
 
 	os.Exit(0)
+}
+
+func deleteOldLogs() {
+	files, err := os.ReadDir("./logs")
+	if err != nil {
+		log.Fatalf("error reading logs directory: %v", err)
+		return
+	}
+
+	cutoffTime := time.Now().AddDate(0, 0, -30) // 30 days ago
+
+	for _, file := range files {
+		info, err := file.Info()
+		if err != nil {
+			log.Printf("error getting file info for log file %s: %v", file.Name(), err)
+			continue
+		}
+
+		if info.ModTime().Before(cutoffTime) {
+			err := os.Remove("./logs/" + file.Name())
+			if err != nil {
+				log.Printf("error deleting old log file %s: %v", file.Name(), err)
+			} else {
+				log.Printf("deleted old log file: %s", file.Name())
+			}
+		}
+	}
 }
